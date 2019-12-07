b83a6c539f91dce384b97460df6fca52d6347491
Zhen Data <zhenyang.9261@gmail.com>
2019-12-06 22:48:59 -0500

    Add js to handle API rountes in routing folder

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+// Load data
+var friendsData = require("../data/friends");
+
+/*
+ * Routes that handle HTML GET and POST requests
+ */
+module.exports = function(app) {
+  // API GET request - send a JSON object to client
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  // API POST request
+  app.post("/api/survey", function(req, res) {});
+};
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+/*
+ * Handle HTML GET requests that result in sending to client HTML pages.
+ */
+
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
