dfd5a2e30f69829fedfe6b2b6b484c5bc1200c63
Zhen Data <zhenyang.9261@gmail.com>
2019-12-10 14:36:10 -0500

    Cleaned up code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,48 +10,40 @@ module.exports = function(app) {
     res.json(friendsData);
   });
 
-  // API POST request
+  // API POST request. Calculation based on data in request body
   app.post("/api/survey", function(req, res) {
     // Convert all strings in array to numbers
     var answerScores = req.body.answers.map(Number);
-    console.log("answer scores: " + answerScores);
-    // The variable to keep track of the index of the friendData and the smallest difference
+
+    // The variables to keep track of the index of the friendData with the smallest difference
     var index = 0;
     var smallestDiff = 0;
 
     // Loop through friendData and compare differences
     for (var i = 0; i < friendsData.length; i++) {
-      // Convert strings to numbers
+      // Convert all strings in array to numbers
       var friendScores = friendsData[i].scores.map(Number);
-      console.log("friendScores: " + friendScores);
 
       var sumDiff = 0;
 
-      // Loop through the answer array and calculate absolute difference of each number
+      // Loop through the answer array and calculate absolute difference with the score array in friendsData
       for (var j = 0; j < friendScores.length; j++) {
         var diff = Math.abs(friendScores[j] - answerScores[j]);
-        console.log(
-          `friend score j: ${friendScores[j]}   answerScores: ${answerScores[j]}`
-        );
         sumDiff += diff;
       }
-      console.log(
-        `smallest diff: ${smallestDiff}  current su diff: ${sumDiff}`
-      );
 
       // Initiate the smallest diff with the very first element's diff
       if (i === 0) {
         smallestDiff = sumDiff;
       }
 
+      // Always keep the smaller value
       if (sumDiff < smallestDiff) {
         smallestDiff = sumDiff;
         index = i;
-        console.log(`index: ${index}`);
       }
     }
 
-    console.log(friendsData[index]);
     res.json(friendsData[index]);
   });
 };
