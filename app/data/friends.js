11860f90daab01f42572bb61301f08649fe0c97f
Zhen Data <zhenyang.9261@gmail.com>
2019-12-09 23:06:01 -0500

    Added real data to fill out modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,57 +1,68 @@
 var friends = [
   {
-    name: "Ahmed",
-    photo: "https://google.com",
+    name: "Ryan Doe",
+    photo:
+      "https://images.unsplash.com/photo-1526149217525-7035aed11799?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
   },
   {
-    name: "Jacob Deming",
-    photo: "https://google.com",
+    name: "Austin Doe",
+    photo:
+      "https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   },
   {
-    name: "Jeremiah Scanlon",
-    photo: "https://google.com",
+    name: "Tom Doe",
+    photo:
+      "https://images.unsplash.com/photo-1561893836-adae6b766f82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
   },
   {
-    name: "Louis T. Delia",
-    photo: "https://google.com",
+    name: "Justin Doe",
+    photo:
+      "https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
   },
   {
-    name: "Lou Ritter",
-    photo: "https://google.com",
+    name: "Jennifer Doe",
+    photo:
+      "https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
   },
   {
-    name: "Jordan Biason",
-    photo: "https://google.com",
+    name: "Grace Doe",
+    photo:
+      "https://images.unsplash.com/photo-1542981533-e288c4fda66d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
   },
   {
-    name: "James meyer",
-    photo: "https://google.com",
+    name: "Helen Doe",
+    photo:
+      "https://images.unsplash.com/photo-1440589473619-3cde28941638?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["3", "3", "5", "3", "5", "3", "5", "4", "2", "3"]
   },
   {
-    name: "Shrek",
-    photo: "https://google.com",
+    name: "Emily Doe",
+    photo:
+      "https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["1", "5", "4", "5", "1", "3", "3", "2", "4", "1"]
   },
   {
-    name: "Grumpy Cat",
-    photo: "https://google.com",
+    name: "Beth Doe",
+    photo:
+      "https://images.unsplash.com/photo-1492106087820-71f1a00d2b11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["1", "5", "4", "5", "1", "1", "3", "2", "4", "1"]
   },
   {
-    name: "John",
-    photo: "https://google.com",
+    name: "Mary Doe",
+    photo:
+      "https://images.unsplash.com/photo-1485811661309-ab85183a729c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["1", "5", "3", "1", "1", "4", "2", "4", "2", "2"]
   },
   {
-    name: "123",
-    photo: "https://google.com",
+    name: "John Doe",
+    photo:
+      "https://images.unsplash.com/photo-1492447166138-50c3889fccb1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     scores: ["2", "3", "1", "2", "2", "4", "2", "4", "4", "2"]
   }
 ];
